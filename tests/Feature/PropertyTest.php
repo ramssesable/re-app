fbb0d9f9c333996844a5a9f2c18e505bef717818
Ramsses Gomez <ramssesable13@gmail.com>
2023-08-21 18:44:01 -0700

    Test for property

---
+++ tests/Feature/PropertyTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PropertyTest extends TestCase
+{
+    /**
+     * Add a house type record without bathrooms.
+     */
+    public function testHouseWithoutBathroomTest()
+    {
+        $payload = [
+            "name"              => "John Doe",
+            "real_state_type"   => "house",
+            "street"            => "Martin Luther King St",
+            "external_number"   => "318",
+            "internal_number"   => "",
+            "neighborhood"      => "75455",
+            "city"              => "Mount Pleasant",
+            "country"           => "CA",
+            "rooms"             => "3",
+            "bathrooms"         => "0",
+            "comments"          => "Located in the Historical area of Pittsburg is this cute 3 bedroom and 1 bath home.",
+        ];
+
+        $this->json('post', '/api/properties', $payload)
+            ->assertStatus(400)
+            ->assertJson([
+                "status"=> false,
+                "errors"=> [
+                    "The bathrooms must be at least 1."
+                ]
+            ]);
+
+    }
+}
