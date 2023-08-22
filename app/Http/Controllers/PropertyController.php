3a4fabdd6ad5218c73aefc0eecbc81272b2cd65f
Ramsses Gomez <ramssesable13@gmail.com>
2023-08-21 19:24:40 -0700

    Deleted record on destroy method

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -93,7 +93,7 @@ class PropertyController extends Controller
         $property = Property::find($id);
 
         if ($property && $property->delete()) {
-            return response()->json('Property deleted successfully');
+            return response()->json($property);
         } else {
             return response()->json([
                 'status' => false,
